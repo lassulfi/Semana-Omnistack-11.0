d3262fd689d7d0750f3ffbedcad462d59e60043e
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-04-05 19:51:13 -0300

    Finalizada suite de tests das ongs

--- backend/tests/unit/repository/OngRepository.spec.js
+++ backend/tests/unit/repository/OngRepository.spec.js
@@ -1,6 +1,10 @@
 const connection = require('../../../src/database/connection');
 const repository = require('../../../src/repository/OngRepository')
 
+let ong = {};
+let ongs = null;
+let id = null;
+
 describe('OngRepository', () => {
     beforeEach(async () => {
         await connection.migrate.rollback();
@@ -12,22 +16,67 @@ describe('OngRepository', () => {
     })
 
     it('should be able to save a new ONG', async () => {
-        const ong = {
-            name: 'ONG para Teste',
-            email: 'contato@ongparateste.com.br',
-            whatsapp: '119988881111',
-            city: 'Sao Paulo',
-            uf: 'SP'
-        }
-        
-        const { id } = await repository.insert(ong);
-        
-        expect(id).toHaveLength(8);
+        givenWheHaveAValidOng();
+        await whenCallRepositoryInsertMethod();
+        thenWeExpectToHaveAValidId();
+        thenWeExpectIdToHaveLengthEqualsEight();        
     }),
 
     it('should be able to list all ongs', async () => {
-        const ongs = await repository.findAll();
-
-        expect(ongs).toBeDefined();
+        givenWheHaveAValidOng();
+        await givenWeHaveOngsInTheDatabase();
+        await whenWeCallRepositoryFindAllMethod();
+        thenWeExpectToHaveAValidList();
+        thenWeExpectAListWithSizeEqualsOne();
     })
 })
+
+/**
+ * Given Methods
+ */
+function givenWheHaveAValidOng() {
+    ong = {
+        name: 'ONG para Teste',
+        email: 'contato@ongparateste.com.br',
+        whatsapp: '119988881111',
+        city: 'Sao Paulo',
+        uf: 'SP'
+    }
+}
+
+async function givenWeHaveOngsInTheDatabase(){
+    await repository.insert(ong);
+}
+
+ /**
+  * When methods
+  */
+
+  async function whenCallRepositoryInsertMethod() {
+      result = await repository.insert(ong);
+      id = result.id;
+  }
+
+  async function whenWeCallRepositoryFindAllMethod() {
+      ongs = await repository.findAll();
+  }
+
+  /**
+   * Then methods
+   */
+
+function thenWeExpectToHaveAValidId() {
+    expect(id).toBeDefined();
+}
+
+function thenWeExpectIdToHaveLengthEqualsEight() {
+    expect(id).toHaveLength(8);
+}
+
+function thenWeExpectToHaveAValidList(){
+    expect(ongs).toBeDefined();
+}
+
+function thenWeExpectAListWithSizeEqualsOne() {
+    expect(ongs).toHaveLength(1);
+}
--- backend/tests/unit/service/OngService.spec.js
+++ backend/tests/unit/service/OngService.spec.js
@@ -2,34 +2,165 @@ const service = require('../../../src/service/OngService');
 const repository = require('../../../src/repository/OngRepository');
 const generateUniqueId = require('../../../src/utils/generateUniqueId');
 
-const testOng = {
-    name: 'ONG para Teste',
-    email: 'contato@ongparateste.com.br',
-    whatsapp: '119988881111',
-    city: 'Sao Paulo',
-    uf: 'SP'
-}
+let mock = null;
+
+let ong = null;
+
+let id = null;
+
+let ongs = null;
 
 describe('OngService', () => {
+    beforeEach(() => {
+        mock = null;
+        id = null;
+        ong = null;
+        ongs = null;
+    });
+
     it('should be able to create a new ong', async () => {
-        const mock = jest.spyOn(repository, 'insert');
 
-        mock.mockImplementation(() => ({ id: generateUniqueId() }));
-
-        const { id } = await service.create(testOng);
-        
-        expect(id).toHaveLength(8);
-        expect(repository.insert).toHaveBeenCalled();
-
-        mock.mockRestore();
+        givenWeHaveAValidOng();
+        givenWeWantToSaveAWithSuccesOng();
+        givenWeHaveAValidId();
+        await whenWeCallServiceInsertMethod()
+        thenWeExpectRepositoryInsertMethodToBeCalled();
+        thenWheExpectToHaveAValidId();
+        thenWeExpectIdToHaveLenghtEqualsEight();
+        thenWeExpectMockToBeRestored();
     }),
-    it('should throw error if an error occurr with database connection', async () => {
-        const mock = jest.spyOn(repository, 'insert');
 
-        mock.mockImplementation(() => new Error('Erro ao persistir ong.'));
+    it('should throw error when inserting if an error occurr with database connection', async () => {
+        givenWeHaveAValidOng();
+        givenWeWantToSaveAWithSuccesOng();
+        givenRepositoryThrowsAnError();
+        whenWeCallServiceInsertMethod();
+        thenWeExpectRepositoryInsertMethodToBeCalled();  
+        thenWeExpectAnInvalidId();
+        thenWeExpectMockToBeRestored();
+    }),
 
-        const { id } = await service.create(testOng);
-        
-        expect(id).toBeUndefined();
+    it('should be able to return a list of ongs', async () => {
+        givenWeHaveAValidOngList();
+        givenWeWantToRetrieveAnOngsList();
+        givenRepositoryRetunsAListOfOng();
+        whenWeCallServiceFindAllMethod();
+        thenWeExpectRepositoryFindAllMethodToBeCalled();
+        thenWeExpectToHaveAValidOngList();
+        thenWeExpectTheOngListToHaveLengthValueOfTwo();
+    }),
+
+    it('should throw an error when retrieving the list of ongs if an error occurr with the connection', async () => {
+        givenWeWantToRetrieveAnOngsList();
+        givenRepositoryThrowsAnError();
+        whenWeCallServiceFindAllMethod();
+        thenWeExpectRepositoryFindAllMethodToBeCalled();
+        thenWeExpectToHaveAnInvalidList();
     })
 });
+
+/**
+ * Given Methods
+ */
+
+function givenWeHaveAValidOng() {
+    ong = {
+        name: 'ONG para Teste',
+        email: 'contato@ongparateste.com.br',
+        whatsapp: '119988881111',
+        city: 'Sao Paulo',
+        uf: 'SP'
+    }
+}
+
+function givenWeHaveAValidId() {
+    mock.mockImplementation(() => ({ id: generateUniqueId() }));
+}
+
+function givenWeWantToSaveAWithSuccesOng(){
+    mock = jest.spyOn(repository, 'insert');
+}
+
+function givenRepositoryThrowsAnError() {
+    mock.mockImplementation(() => {throw new Error('Erro ao persistir ong.')});
+}
+
+function givenWeHaveAValidOngList(){
+    ongs = [
+        {
+            name: 'ONG para Teste 1',
+            email: 'contato@ongparateste1.com.br',
+            whatsapp: '119988881111',
+            city: 'Sao Paulo',
+            uf: 'SP'
+        },
+        {
+            name: 'ONG para Teste 2',
+            email: 'contato@ongparateste2.com.br',
+            whatsapp: '119988881111',
+            city: 'Sao Paulo',
+            uf: 'SP'
+        }
+    ]
+}
+
+function givenWeWantToRetrieveAnOngsList() {
+    mock = jest.spyOn(repository, 'findAll');
+}
+
+function givenRepositoryRetunsAListOfOng(){
+    mock.mockImplementation(() => (ongs));
+}
+
+/**
+ * When Methods
+ */
+
+async function whenWeCallServiceInsertMethod() {
+    const result = await service.create(ong);
+    id = result.id;
+}
+
+async function whenWeCallServiceFindAllMethod() {
+    ongs = await service.findAll();
+}
+
+ /**
+  * Then Methods
+  */
+
+  function thenWheExpectToHaveAValidId() {
+      expect(id).toBeDefined();
+  }
+
+  function thenWeExpectIdToHaveLenghtEqualsEight(){
+      expect(id).toHaveLength(8);
+  }
+
+  function thenWeExpectRepositoryInsertMethodToBeCalled(){
+    expect(repository.insert).toHaveBeenCalled();
+  }
+
+  function thenWeExpectAnInvalidId(){
+    expect(id).toBeNull();
+  }
+
+  function thenWeExpectRepositoryFindAllMethodToBeCalled() {
+      expect(repository.findAll).toHaveBeenCalled();
+  }
+
+  function thenWeExpectToHaveAValidOngList() {
+      expect(ongs).toBeDefined();
+  }
+
+  function thenWeExpectTheOngListToHaveLengthValueOfTwo() {
+      expect(ongs).toHaveLength(2);
+  }
+
+  function thenWeExpectToHaveAnInvalidList(){
+      expect(ongs).toBeNull();
+  }
+
+  function thenWeExpectMockToBeRestored() {
+      if(mock) mock.mockRestore();
+  }
